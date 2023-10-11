bbbb35a58ea7aacee8bc6716b02af777a04d843e
KukCH <knkn9436@naver.com>
2023-10-12 01:06:42 +0900

    Fix. MapStruct를 활용해 EntityList->DTOList Mapping시 Entity->DTO mapping 메서드 이용하도록 수정

--- Board/src/main/java/com/viper/board/service/model/mapper/ReplyMapper.java
+++ Board/src/main/java/com/viper/board/service/model/mapper/ReplyMapper.java
@@ -5,6 +5,7 @@ import com.viper.board.persistence.entity.Reply;
 import com.viper.board.persistence.entity.User;
 import com.viper.board.service.model.dto.req.ReplyReqDTO;
 import com.viper.board.service.model.dto.res.ReplyResDTO;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -19,12 +20,15 @@ public interface ReplyMapper {
     @Mapping(target = "content", source = "create.content")
     Reply toReplyEntity(ReplyReqDTO.CREATE create, User user, Board board);
 
-//    @Mapping(target = "id", source = "id")
-//    @Mapping(target = "writerId", source = "reply.user", qualifiedByName = "mapUserToUserId")
-//    @Mapping(target = "boardId", source = "reply.board", qualifiedByName = "mapBoardToBoardId")
-//    ReplyResDTO.READ toReadDto(Reply reply);
+    @Named("E2R")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "writerId", source = "reply.user", qualifiedByName = "mapUserToUserId")
+    @Mapping(target = "boardId", source = "reply.board", qualifiedByName = "mapBoardToBoardId")
+    ReplyResDTO.READ toReadDto(Reply reply);
 
+    @IterableMapping(qualifiedByName = "E2R")
     List<ReplyResDTO.READ> toReadDtoList(List<Reply> replyList);
+
     @Named("mapUserToUserId")
     default Long mapUserToUserId(User user) {
         return user.getId();
